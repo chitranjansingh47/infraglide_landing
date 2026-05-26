60fb40c1ceb8160857ba70c6af1afd1d521fa1e5
chitranjansingh47 <chitranjan.22jics211@jietjodhpur.ac.in>
2026-05-26 23:10:50 +0530

    deployment ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    netlify(),
     react(),
     tailwindcss(),
     tsConfigPaths(),
